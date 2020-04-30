265a852b649c345863e9e689ed73e3975e317cc9
RobiLono <robilono@hawaii.edu>
2020-04-29 23:21:11 -1100

    not done

---
+++ Lono_Robi_Assignment2/public/product_data.js
@@ -0,0 +1,28 @@
+//* used template of code from SmartPhone products
+var products =
+[
+    {  
+    "brand":"Milk-Bones",  
+    "price": 5.50,  
+    "image": './images/milkbones.jpg'
+    },
+    {  
+    "brand":"SmartBones",  
+    "price": 10.25,  
+    "image": './images/smartbones.jpg'
+    },
+    {  
+    "brand":"Bits",  
+    "price": 8.45,  
+    "image": './images/bits.jpg'
+    },
+    {  
+     "brand":"Mammoth-Bones",  
+     "price": 13.75,  
+     "image": './images/mammoth_bones.jpg'
+    }
+
+  ];
+ 
+  if(typeof module != 'undefined') {
+    module.exports.products = products};
